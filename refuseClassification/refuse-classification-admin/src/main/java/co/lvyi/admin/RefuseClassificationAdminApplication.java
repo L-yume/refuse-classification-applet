2101eb1b0af9f0829182454559370e18a867b885
Charlie <lxfghome@gmail.com>
2023-03-30 14:44:40 +0800

    后台管理接口模块...

---
+++ refuseClassification/refuse-classification-admin/src/main/java/co/lvyi/admin/RefuseClassificationAdminApplication.java
@@ -0,0 +1,13 @@
+package co.lvyi.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefuseClassificationAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefuseClassificationAdminApplication.class, args);
+    }
+
+}
