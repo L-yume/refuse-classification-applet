5089bd0be32c3acfbb58c41bdb8ca351dc15ffde
Charlie <lxfghome@gmail.com>
2023-04-01 23:31:58 +0800

    加入doc文件夹用于放项目文档;新建bean模块...

---
+++ refuseClassification/refuse-classification-bean/src/test/java/co/lvyi/bean/RefuseClassificationBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package co.lvyi.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefuseClassificationBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
