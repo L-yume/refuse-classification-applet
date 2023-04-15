7b4fb27a01bb368ae829b79e2d25d109e8e8f811
Charlie <lxfghome@gmail.com>
2023-04-15 23:49:43 +0800

    完成侧边导航栏展示...

--- refuseClassification/refuse-classification-admin/src/main/java/co/lvyi/controller/SysLoginController.java
+++ refuseClassification/refuse-classification-admin/src/main/java/co/lvyi/controller/SysLoginController.java
@@ -9,16 +9,21 @@
 package co.lvyi.controller;
 
 import co.lvyi.bean.admin.dto.LoginDTO;
+import co.lvyi.bean.admin.entity.SysMenu;
+import co.lvyi.bean.admin.entity.SysUser;
 import co.lvyi.common.constant.Constants;
 import co.lvyi.common.restful.JsonResult;
+import co.lvyi.common.utils.SecurityUtils;
 import co.lvyi.security.service.SysLoginService;
+import co.lvyi.security.service.SysPermissionService;
+import co.lvyi.system.service.ISysMenuService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * 登录验证
@@ -28,8 +33,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Api(tags = "管理员登录功能")
 public class SysLoginController {
+
     @Autowired
     private SysLoginService sysLoginService;
+
+    @Autowired
+    private ISysMenuService menuService;
+
+    @Autowired
+    private SysPermissionService permissionService;
 
     @PostMapping("/login")
     public JsonResult login(@RequestBody LoginDTO loginDTO){
@@ -40,6 +52,43 @@ public class SysLoginController {
                 loginDTO.getCode(), loginDTO.getUuid());
         result.put(Constants.TOKEN, token);
         return result;
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @return 用户信息
+     */
+    @GetMapping("getInfo")
+    public JsonResult getInfo()
+    {
+        log.info("获取用户信息...");
+        SysUser user = SecurityUtils.getLoginUser().getUser();
+        // 角色集合
+        Set<String> roles = permissionService.getRolePermission(user);
+        // 权限集合
+        Set<String> permissions = permissionService.getMenuPermission(user);
+        JsonResult ajax = JsonResult.success();
+        ajax.put("user", user);
+        ajax.put("roles", roles);
+        ajax.put("permissions", permissions);
+        log.info("用户信息:"+ajax);
+        return ajax;
+    }
+
+    /**
+     * 获取路由信息
+     *
+     * @return 路由信息
+     */
+    @GetMapping("getRouters")
+    public JsonResult getRouters()
+    {
+        log.info("获取路由信息...");
+        Long userId = SecurityUtils.getUserId();
+        List<SysMenu> menus = menuService.selectMenuTreeByUserId(userId);
+        log.info("路由信息:"+JsonResult.success(menuService.buildMenus(menus)));
+        return JsonResult.success(menuService.buildMenus(menus));
     }
 }
 
--- refuseClassification/refuse-classification-system/src/main/java/co/lvyi/system/service/impl/SysMenuServiceImpl.java
+++ refuseClassification/refuse-classification-system/src/main/java/co/lvyi/system/service/impl/SysMenuServiceImpl.java
@@ -11,10 +11,13 @@ package co.lvyi.system.service.impl;
 import co.lvyi.bean.admin.entity.SysMenu;
 import co.lvyi.bean.admin.entity.SysUser;
 import co.lvyi.bean.admin.entity.TreeSelect;
+import co.lvyi.common.constant.Constants;
+import co.lvyi.common.constant.UserConstants;
 import co.lvyi.common.utils.SecurityUtils;
 import co.lvyi.common.utils.StringUtils;
 import co.lvyi.system.mapper.SysMenuMapper;
 import co.lvyi.system.service.ISysMenuService;
+import co.lvyi.system.vo.MetaVo;
 import co.lvyi.system.vo.RouterVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -133,9 +136,62 @@ public class SysMenuServiceImpl implements ISysMenuService {
         return null;
     }
 
+    /**
+     * 构建前端路由所需要的菜单
+     *
+     * @param menus 菜单列表
+     * @return 路由列表
+     */
     @Override
-    public List<RouterVo> buildMenus(List<SysMenu> menus) {
-        return null;
+    public List<RouterVo> buildMenus(List<SysMenu> menus)
+    {
+        List<RouterVo> routers = new LinkedList<RouterVo>();
+        for (SysMenu menu : menus)
+        {
+            RouterVo router = new RouterVo();
+            router.setHidden("1".equals(menu.getVisible()));
+            router.setName(getRouteName(menu));
+            router.setPath(getRouterPath(menu));
+            router.setComponent(getComponent(menu));
+            router.setQuery(menu.getQuery());
+            router.setMeta(new MetaVo(menu.getMenuName(), menu.getIcon(), StringUtils.equals("1", menu.getIsCache()), menu.getPath()));
+            List<SysMenu> cMenus = menu.getChildren();
+            if (StringUtils.isNotEmpty(cMenus) && UserConstants.TYPE_DIR.equals(menu.getMenuType()))
+            {
+                router.setAlwaysShow(true);
+                router.setRedirect("noRedirect");
+                router.setChildren(buildMenus(cMenus));
+            }
+            else if (isMenuFrame(menu))
+            {
+                router.setMeta(null);
+                List<RouterVo> childrenList = new ArrayList<RouterVo>();
+                RouterVo children = new RouterVo();
+                children.setPath(menu.getPath());
+                children.setComponent(menu.getComponent());
+                children.setName(StringUtils.capitalize(menu.getPath()));
+                children.setMeta(new MetaVo(menu.getMenuName(), menu.getIcon(), StringUtils.equals("1", menu.getIsCache()), menu.getPath()));
+                children.setQuery(menu.getQuery());
+                childrenList.add(children);
+                router.setChildren(childrenList);
+            }
+            else if (menu.getParentId().intValue() == 0 && isInnerLink(menu))
+            {
+                router.setMeta(new MetaVo(menu.getMenuName(), menu.getIcon()));
+                router.setPath("/");
+                List<RouterVo> childrenList = new ArrayList<RouterVo>();
+                RouterVo children = new RouterVo();
+                String routerPath = innerLinkReplaceEach(menu.getPath());
+                children.setPath(routerPath);
+                children.setComponent(UserConstants.INNER_LINK);
+                children.setName(StringUtils.capitalize(routerPath));
+                children.setMeta(new MetaVo(menu.getMenuName(), menu.getIcon(), menu.getPath()));
+                childrenList.add(children);
+                router.setChildren(childrenList);
+            }
+            routers.add(router);
+        }
+        return routers;
     }
 
     @Override
@@ -180,6 +236,109 @@ public class SysMenuServiceImpl implements ISysMenuService {
 
     @Override
     public boolean checkMenuNameUnique(SysMenu menu) {return false;}
+
+    /**
+     * 获取路由名称
+     *
+     * @param menu 菜单信息
+     * @return 路由名称
+     */
+    public String getRouteName(SysMenu menu)
+    {
+        String routerName = StringUtils.capitalize(menu.getPath());
+        // 非外链并且是一级目录（类型为目录）
+        if (isMenuFrame(menu))
+        {
+            routerName = StringUtils.EMPTY;
+        }
+        return routerName;
+    }
+
+    /**
+     * 是否为菜单内部跳转
+     *
+     * @param menu 菜单信息
+     * @return 结果
+     */
+    public boolean isMenuFrame(SysMenu menu)
+    {
+        return menu.getParentId().intValue() == 0 && UserConstants.TYPE_MENU.equals(menu.getMenuType())
+                && menu.getIsFrame().equals(UserConstants.NO_FRAME);
+    }
+
+    /**
+     * 获取路由地址
+     *
+     * @param menu 菜单信息
+     * @return 路由地址
+     */
+    public String getRouterPath(SysMenu menu)
+    {
+        String routerPath = menu.getPath();
+        // 内链打开外网方式
+        if (menu.getParentId().intValue() != 0 && isInnerLink(menu))
+        {
+            routerPath = innerLinkReplaceEach(routerPath);
+        }
+        // 非外链并且是一级目录（类型为目录）
+        if (0 == menu.getParentId().intValue() && UserConstants.TYPE_DIR.equals(menu.getMenuType())
+                && UserConstants.NO_FRAME.equals(menu.getIsFrame()))
+        {
+            routerPath = "/" + menu.getPath();
+        }
+        // 非外链并且是一级目录（类型为菜单）
+        else if (isMenuFrame(menu))
+        {
+            routerPath = "/";
+        }
+        return routerPath;
+    }
+
+    /**
+     * 获取组件信息
+     *
+     * @param menu 菜单信息
+     * @return 组件信息
+     */
+    public String getComponent(SysMenu menu)
+    {
+        String component = UserConstants.LAYOUT;
+        if (StringUtils.isNotEmpty(menu.getComponent()) && !isMenuFrame(menu))
+        {
+            component = menu.getComponent();
+        }
+        else if (StringUtils.isEmpty(menu.getComponent()) && menu.getParentId().intValue() != 0 && isInnerLink(menu))
+        {
+            component = UserConstants.INNER_LINK;
+        }
+        else if (StringUtils.isEmpty(menu.getComponent()) && isParentView(menu))
+        {
+            component = UserConstants.PARENT_VIEW;
+        }
+        return component;
+    }
+
+    /**
+     * 是否为内链组件
+     *
+     * @param menu 菜单信息
+     * @return 结果
+     */
+    public boolean isInnerLink(SysMenu menu)
+    {
+        return menu.getIsFrame().equals(UserConstants.NO_FRAME) && StringUtils.ishttp(menu.getPath());
+    }
+
+    /**
+     * 是否为parent_view组件
+     *
+     * @param menu 菜单信息
+     * @return 结果
+     */
+    public boolean isParentView(SysMenu menu)
+    {
+        return menu.getParentId().intValue() != 0 && UserConstants.TYPE_DIR.equals(menu.getMenuType());
+    }
 
     /**
      * 根据父节点的ID获取所有子节点
@@ -245,9 +404,17 @@ public class SysMenuServiceImpl implements ISysMenuService {
     /**
      * 判断是否有子节点
      */
-    private boolean hasChild(List<SysMenu> list, SysMenu t)
+    private boolean hasChild(List<SysMenu> list, SysMenu t) { return getChildList(list, t).size() > 0; }
+
+    /**
+     * 内链域名特殊字符替换
+     *
+     * @return 替换后的内链域名
+     */
+    public String innerLinkReplaceEach(String path)
     {
-        return getChildList(list, t).size() > 0;
+        return StringUtils.replaceEach(path, new String[] { Constants.HTTP, Constants.HTTPS, Constants.WWW, "." },
+                new String[] { "", "", "", "/" });
     }
 }
 
